a541335414fde1dd1f2ee59f9d5b5d99d4452fb5
Emma J <codemzy@gmail.com>
2016-02-29 15:06:23 +0000

    Made chart responsive

--- public/app/app.js
+++ public/app/app.js
@@ -107,6 +107,6 @@ angular.module('StocksRockApp', [])
             datasetFill : true,
             legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>",
             responsive: true,
-            maintainAspectRatio: true
+            maintainAspectRatio: false
         };
     }]);
